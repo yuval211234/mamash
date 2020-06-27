2c7e5e7c2eac207b589a9c46685598b68b56654c
Daniel Toplian <danieltoplian@gmail.com>
2020-06-28 02:30:17 +0300

    fix counter component

--- mamash-client/src/counter/index.jsx
+++ mamash-client/src/counter/index.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import Typography from '@material-ui/core/Typography';
+import { SOLDIER_STATUS } from '../global';
 
 
 const teamSoldierCounter = (soldiers) => {
-    return soldiers.filter(soldier => soldier.isHere == true).length;
+    return soldiers.filter(soldier => soldier.status === SOLDIER_STATUS.HERE).length;
 }
 
 const Counter = ({soldierList}) => {
